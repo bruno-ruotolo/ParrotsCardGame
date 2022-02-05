480c33cb34d3fbcde5dfded5b2e55daceb1ad9d7
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-02-05 19:53:11 -0300

    Randomização das Cartas e Formação de Pares

--- script.js
+++ script.js
@@ -11,25 +11,51 @@ function cardRotation(element) {
     element.classList.add("rotate");
 }
 
-function addCardToScreen(cardAmount) {
-    const parrotsGifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
+let parrotsGifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
+parrotsGifs.sort(randomNumber);
+
+
+function randomNumber() {
+    return Math.random() - 0.5;
+}
+
+
+let randoArrayNumber = [];
+let sorter = []; // para ver no console
+function sorterCardAmount(amount) {
+    for (let i = 0; i < amount / 2; i++) {
+        sorter.push(i);
+    }
+    for (let i = 0; i < amount / 2; i++) {
+        sorter.push(i);
+    }
+
+    sorter.sort(randomNumber);
+}
+
+
+function addCardToScreen() {
     const cardsContainer = document.querySelector(".cards");
+    sorterCardAmount(cardAmount)
     for (let i = 0; i < cardAmount; i++) {
         cardsContainer.innerHTML += ` 
-    <div class="card" onclick="cardRotation(this)">
-        <div class="front-face face">
-            <img src="Parrots Gifs/front.png" alt="Frente da Carta">
-        </div>
+        <div class="card" onclick="cardRotation(this)">
+            <div class="front-face face">
+                <img src="Parrots Gifs/front.png" alt="Frente da Carta">
+            </div>
 
-        <div class="back-face face">
-            <img src="Parrots Gifs/${parrotsGifs[i]}" alt="unicorn parrot">
-        </div>
-    </div>
-    <div class="`
+            <div class="back-face face">
+                <img src="Parrots Gifs/${parrotsGifs[(sorter[i])]}" alt="unicorn parrot">
+            </div>
+        </div>`
+
     }
+
 }
 
 
 
+
+
 askCardAmount();
-addCardToScreen(cardAmount);
+addCardToScreen();
