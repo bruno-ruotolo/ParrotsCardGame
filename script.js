ed8deac2b73187dd16aa57a0fa6e460c1c6ba067
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-02-05 23:04:28 -0300

    Aplicação das ambas as situações possiveis ao selecionar duas cartas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let cardAmount = null;
+let movesCounter = 0;
+
 function askCardAmount() {
     cardAmount = parseInt(prompt("Quantas cartas você deseja? (números pares de 4 à 14)"));
 
@@ -7,9 +9,51 @@ function askCardAmount() {
     }
 }
 
+let cardContainerFirstClassName = null;
+let cardContainerSecondClassName = null;
+let cardContainerFirst = null;
+let cardContainerSecond = null;
+
 function cardRotation(element) {
     element.classList.add("rotate");
+    movesCounter++;
+    couplesCardsTest(element);
+    setTimeout(rotateWrongCoupleCards, 1000);
 }
+
+function couplesCardsTest(element) {
+
+    if (movesCounter % 2 !== 0) {
+        cardContainerFirst = (element.querySelector(".back-face img"));
+        cardContainerFirstClassName = cardContainerFirst.className;
+    }
+
+    else {
+        cardContainerSecond = (element.querySelector(".back-face img"));
+        cardContainerSecondClassName = cardContainerSecond.className;
+    }
+}
+let rightCoupleCounter = 0;
+function rotateWrongCoupleCards() {
+
+    if (cardContainerSecondClassName !== cardContainerFirstClassName && cardContainerSecond !== null && cardContainerFirst !== null) {
+        cardContainerFirst.parentNode.parentNode.classList.remove("rotate");
+        cardContainerSecond.parentNode.parentNode.classList.remove("rotate");
+        cardContainerFirst = null;
+        cardContainerSecond = null;
+    }
+    else if (cardContainerSecondClassName === cardContainerFirstClassName && cardContainerSecond !== null && cardContainerFirst !== null) {
+        console.log("Acertou");
+        cardContainerFirst = null;
+        cardContainerSecond = null;
+        rightCoupleCounter++;
+    }
+
+    if (rightCoupleCounter === cardAmount / 2) {
+        alert(`Você ganhou em ${movesCounter} jogadas!`);
+    }
+}
+
 
 let parrotsGifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
 parrotsGifs.sort(randomNumber);
@@ -20,7 +64,7 @@ function randomNumber() {
 }
 
 
-let randoArrayNumber = [];
+
 let sorter = []; // para ver no console
 function sorterCardAmount(amount) {
     for (let i = 0; i < amount / 2; i++) {
@@ -36,7 +80,7 @@ function sorterCardAmount(amount) {
 
 function addCardToScreen() {
     const cardsContainer = document.querySelector(".cards");
-    sorterCardAmount(cardAmount)
+    sorterCardAmount(cardAmount);
     for (let i = 0; i < cardAmount; i++) {
         cardsContainer.innerHTML += ` 
         <div class="card" onclick="cardRotation(this)">
@@ -45,7 +89,7 @@ function addCardToScreen() {
             </div>
 
             <div class="back-face face">
-                <img src="Parrots Gifs/${parrotsGifs[(sorter[i])]}" alt="unicorn parrot">
+                <img class= "${parrotsGifs[(sorter[i])]}" src="Parrots Gifs/${parrotsGifs[(sorter[i])]}" alt="unicorn parrot">
             </div>
         </div>`
 
